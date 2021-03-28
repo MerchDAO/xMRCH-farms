1282709aefbd319f410e2fcd707feb117f1ae398
Jack Li <jackz163299@gmail.com>
2021-03-28 15:19:04 -0400

    add mainnet network

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,6 +27,10 @@ module.exports = {
     ropsten: {
       url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.MAINNET_PRIVATE_KEY}`]
     }
   },
   etherscan: {
