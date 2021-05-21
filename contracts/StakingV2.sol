3bf0366257e48a4d7866002bf3a24b5557e4dc9e
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-05-21 17:54:38 +0300

    undo (find error)

--- contracts/StakingV2.sol
+++ contracts/StakingV2.sol
@@ -12,19 +12,14 @@ contract StakingV2 is AccessControl {
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-    /// @notice A checkpoint for marking number of stake tokens from a given block
-    struct Checkpoint {
-        uint32 fromBlock;
-        uint96 amount;
+    struct Stake {
+        address user;
+        uint256 amount;
+        uint64 sinceBlock;
+        uint64 untilBlock;
     }
 
-    /// @notice A record of stake tokens checkpoints for each account, by index
-    mapping (address => mapping (uint32 => Checkpoint)) public checkpoints;
-
-    /// @notice The number of checkpoints for each account
-    mapping (address => uint32) public numCheckpoints;
-
-    mapping (address => uint32) public lastClaim; // block number of last claim for staker
+    mapping(address => Stake) public stakes;
 
     address public stakeToken; // Uniswap LP token from pool MRCH|ETH
     address public rewardToken; // MRCH token
@@ -80,15 +75,7 @@ contract StakingV2 is AccessControl {
 
         doTransferIn(staker, stakeToken, amountIn);
 
-        uint32 stakerNum = numCheckpoints[staker];
-        uint96 stakerOldAmount = stakerNum > 0 ? checkpoints[staker][stakerNum - 1].amount : 0;
-        uint96 stakerNewAmount = add96(stakerOldAmount, uint96(amountIn), "MRCHStaking::stake: staker tokens amount overflows");
-        _writeCheckpoint(staker, stakerNum, stakerOldAmount, stakerNewAmount);
-
-        uint32 contractNum = numCheckpoints[address(this)];
-        uint96 totalOldAmount = stakerNum > 0 ? checkpoints[address(this)][contractNum - 1].amount : 0;
-        uint96 totalNewAmount = add96(totalOldAmount, uint96(amountIn), "MRCHStaking::stake: contract tokens amount overflows");
-        _writeCheckpoint(address(this), contractNum, totalOldAmount, totalNewAmount);
+        // @Todo add data of stake
 
         return true;
     }
@@ -96,7 +83,8 @@ contract StakingV2 is AccessControl {
     function withdraw() public returns (bool) {
         require(claimReward(), "MRCHStaking::withdraw: claim error");
 
-        uint amount = getPriorAmount(msg.sender, block.number);
+        uint amount = 0; // @Todo get all stake amount
+
         return withdrawWithoutReward(amount);
     }
 
@@ -108,15 +96,7 @@ contract StakingV2 is AccessControl {
         require(block.number > startBlockNum, "MRCHStaking::withdrawInternal: bad timing for the request");
         require(amountOut > 0, "MRCHStaking::withdrawInternal: must be positive");
 
-        uint32 stakerNum = numCheckpoints[staker];
-        uint96 stakerOldAmount = stakerNum > 0 ? checkpoints[staker][stakerNum - 1].amount : 0;
-        uint96 stakerNewAmount = sub96(stakerOldAmount, uint96(amountOut), "MRCHStaking::withdrawInternal: staker token amount underflows");
-        _writeCheckpoint(staker, stakerNum, stakerOldAmount, stakerNewAmount);
-
-        uint32 contractNum = numCheckpoints[address(this)];
-        uint96 totalOldAmount = stakerNum > 0 ? checkpoints[address(this)][contractNum - 1].amount : 0;
-        uint96 totalNewAmount = sub96(totalOldAmount, uint96(amountOut), "MRCHStaking::withdrawInternal: contract tokens amount underflows");
-        _writeCheckpoint(address(this), contractNum, totalOldAmount, totalNewAmount);
+        // @Todo change data of stake
 
         doTransferOut(stakeToken, staker, amountOut);
 
@@ -134,8 +114,6 @@ contract StakingV2 is AccessControl {
 
         doTransferOut(rewardToken, staker, rewardAmount);
 
-        lastClaim[staker] = uint32(currentRoundNum());
-
         emit RewardOut(staker, rewardAmount);
 
         return true;
@@ -143,19 +121,8 @@ contract StakingV2 is AccessControl {
 
     function calcReward(address staker) public view returns (uint) {
         uint reward;
-        uint lastRound = lastClaim[staker];
 
-        uint stakerAmount;
-        uint totalAmount;
-        uint endBlockRoundNum;
-
-        for(uint i = lastRound; i < currentRoundNum(); i++) {
-            endBlockRoundNum = startBlockNum.add(roundBlocks * i);
-            stakerAmount = getPriorAmount(staker, endBlockRoundNum);
-            totalAmount = getPriorAmount(address(this), endBlockRoundNum);
-
-            reward = reward.add(roundRewardAmount.mul(stakerAmount).div(totalAmount));
-        }
+        // @Todo calc reward
 
         return reward;
     }
@@ -188,60 +155,6 @@ contract StakingV2 is AccessControl {
 
     function getTimeStamp() public view virtual returns (uint) {
         return block.timestamp;
-    }
-
-    /**
-     * @notice Determine the prior number of stake tokens for an account as of a block number
-     * @dev Block number must be a finalized block or else this function will revert to prevent misinformation.
-     * @param account The address of the account to check
-     * @param blockNumber The block number to get the vote balance at
-     * @return The number of stake tokens the account had as of the given block
-     */
-    function getPriorAmount(address account, uint blockNumber) public view returns (uint96) {
-        require(blockNumber < block.number, "MRCHStaking::getPriorAmount: not yet determined");
-
-        uint32 nCheckpoints = numCheckpoints[account];
-        if (nCheckpoints == 0) {
-            return 0;
-        }
-
-        // First check most recent balance
-        if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
-            return checkpoints[account][nCheckpoints - 1].amount;
-        }
-
-        // Next check implicit zero balance
-        if (checkpoints[account][0].fromBlock > blockNumber) {
-            return 0;
-        }
-
-        uint32 lower = 0;
-        uint32 upper = nCheckpoints - 1;
-        while (upper > lower) {
-            uint32 center = upper - (upper - lower) / 2; // ceil, avoiding overflow
-            Checkpoint memory cp = checkpoints[account][center];
-            if (cp.fromBlock == blockNumber) {
-                return cp.amount;
-            } else if (cp.fromBlock < blockNumber) {
-                lower = center;
-            } else {
-                upper = center - 1;
-            }
-        }
-        return checkpoints[account][lower].amount;
-    }
-
-    function _writeCheckpoint(address user, uint32 nCheckpoints, uint96 oldAmount, uint96 newAmount) internal {
-        uint32 blockNumber = safe32(block.number, "MRCHStaking::_writeCheckpoint: block number exceeds 32 bits");
-
-        if (nCheckpoints > 0 && checkpoints[user][nCheckpoints - 1].fromBlock == blockNumber) {
-            checkpoints[user][nCheckpoints - 1].amount = newAmount;
-        } else {
-            checkpoints[user][nCheckpoints] = Checkpoint(blockNumber, newAmount);
-            numCheckpoints[user] = nCheckpoints + 1;
-        }
-
-        emit StakeAmountChanged(user, oldAmount, newAmount);
     }
 
     function add96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
