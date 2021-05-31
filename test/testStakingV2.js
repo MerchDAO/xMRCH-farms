f008b44b3a7d4fd300a1cda2ab8b6c3d774faac8
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-05-31 13:49:31 +0300

    save progress

--- test/testStakingV2.js
+++ test/testStakingV2.js
@@ -9,7 +9,14 @@ describe("MRCH StakingV2 test", async () => {
     let tokenmrch;
     let tokenlp;
     let staking;
-    let startTime = Math.floor(Date.now() / 1000);
+    let startTime = Math.floor(Date.now() / 1000) + 15;
+    console.log("data", Math.floor(Date.now() / 1000));
+    console.log("startTime", startTime);
+    let endTime = startTime + 1000;
+    console.log("endTime", endTime);
+    let freezeTime = 100;
+    let percent = 5;
+    console.log("freezeTime", freezeTime);
 
     it("STEP 1. Creating MRCH token contract", async function () {
         const TokenMRCH = await hre.ethers.getContractFactory("TokenMRCH");
@@ -36,5 +43,41 @@ describe("MRCH StakingV2 test", async () => {
             tokenmrch.address
         );
     });
+
+    it("STEP 4. Minting", async function () {
+        const [...addr] = await ethers.getSigners();
+        tokenlp.Mint(ethers.utils.parseEther("100000.0"));
+        tokenlp.transfer(addr[1].address, ethers.utils.parseEther("100.0"));
+        tokenlp.transfer(addr[2].address, ethers.utils.parseEther("100.0"));
+        tokenlp.transfer(addr[3].address, ethers.utils.parseEther("100.0"));
+    });
+
+    it("STEP 5. Add pool", async function () {
+        const [...addr] = await ethers.getSigners();
+
+        let balance = await tokenmrch.balanceOf(addr[0].address);
+        console.log('balance', balance / 1e18);
+
+        await tokenmrch.approve(staking.address, ethers.utils.parseEther("1000.0"));
+
+        let allowance = await tokenmrch.allowance(addr[0].address, staking.address);
+        console.log('allowance', allowance / 1e18);
+
+        let check = await staking.getTimeStamp();
+        console.log('check2', check.toString());
+
+        staking.addPool(ethers.utils.parseEther("1000.0"), startTime, endTime, freezeTime, percent);
+    });
+
+    it("STEP 6. Staking", async function () {
+        const [...addr] = await ethers.getSigners();
+
+    });
+
+    it("STEP 7. Unstaking", async function () {
+        const [...addr] = await ethers.getSigners();
+
+    });
+
 
 });
